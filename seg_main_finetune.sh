75ac8f025fa788c792bbc1d2b57bfd6b69ffc232
Wu Youxin <96423839+traptinblur@users.noreply.github.com>
2023-07-13 16:45:05 +0800

    Delete seg_main_finetune.sh

--- seg_main_finetune.sh
+++
@@ -1,3 +0,0 @@
-echo "Distributed Segmentation Model Training"
-CUDA_VISIBLE_DEVICES=4,5,6,7 PYTHONPATH=. python -m torch.distributed.launch  --nproc_per_node=4 --master_port=9971 seg_main_finetune.py \
- --dist_eval
