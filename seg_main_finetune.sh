e00781b76b4ae0406d9987e4bed7271126ed20fd
Wu Youxin <96423839+traptinblur@users.noreply.github.com>
2023-07-13 18:41:27 +0800

    Update seg_main_finetune.sh

--- seg_main_finetune.sh
+++ seg_main_finetune.sh
@@ -1,4 +1,4 @@
 echo "Distributed Segmentation Model Training"
-CUDA_VISIBLE_DEVICES=4,5,6,7 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
 python -m torch.distributed.launch  --nproc_per_node=4 --master_port=9971 \
     seg_main_finetune.py --config base_config --dist_eval
